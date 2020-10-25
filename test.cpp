d4ae8aad9de8133bea80f00a3ee5f41eeaca891a
Soumyadip Saha <soumyadip.saha@iiitb.org>
2020-10-25 22:02:02 +0000

    formatted

--- test.cpp
+++ test.cpp
@@ -1,36 +1,56 @@
-// Problem Link - 
+// Problem Link -
 /* By Sanket Singh */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 //#include<ext/pb_ds/assoc_container.hpp>
 //using namespace __gnu_pbds;
 using namespace std;
-#define ll 				long long int
-#define ld				long double
-#define mod             1000000007
-#define inf             1e18
-#define endl			"\n"
-#define pb 				emplace_back
-#define vi              vector<ll>
-#define vs				vector<string>
-#define pii             pair<ll,ll>
-#define ump				unordered_map
-#define mp 				map
-#define pq_max          priority_queue<ll>
-#define pq_min          priority_queue<ll,vi,greater<ll> >
-#define ff 				first
-#define ss 				second
-#define mid(l,r)        (l+(r-l)/2)
-#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
-#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
-#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
-#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
-vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
-
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb emplace_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+#define token(str, ch) (std::istringstream var((str)); vs v; string t; while (getline((var), t, (ch))) { v.pb(t); } return v;)
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
 
 void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
     cout << *it << " = " << a << endl;
     err(++it, args...);
 }
@@ -38,16 +58,17 @@ void err(istream_iterator<string> it, T a, Args... args) {
 
 void file_i_o()
 {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     file_i_o();
     // string str = "codechef";
     // string str1("unacademy");
@@ -91,11 +112,13 @@ int main(int argc, char const *argv[]) {
     // cout<< (a == b) <<" "<<(a == c);
 
     // string tokenization
-    string str; cin>>str;
+    string str;
+    cin >> str;
     istringstream var(str);
     string token;
-    while(getline(var, token, '-')) {
-        cout<<token<<"\n";
+    while (getline(var, token, '-'))
+    {
+        cout << token << "\n";
     }
     return 0;
 }
