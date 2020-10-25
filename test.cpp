22465ccfe5761471b53b021013ee93524031f311
Soumyadip Saha <soumyadip.saha@iiitb.org>
2020-10-25 06:23:30 +0000

    Test commit

--- test.cpp
+++ test.cpp
@@ -23,7 +23,7 @@ using namespace std;
 #define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
 #define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
 #define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define logarr(arr,a,b)	for(int i=(a);i<=(b);i++) cout<<(arr[i])<<" ";cout<<endl;	
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
 #define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
 vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
 
@@ -49,46 +49,53 @@ void file_i_o()
 
 int main(int argc, char const *argv[]) {
     file_i_o();
-    vector<int> v;
-    log(v.size(), v.capacity());
-    for(int i =0 ; i < 10; i++) {
-        v.push_back(i);
-        log(v.size(), v.capacity());
-    }
+    // string str = "codechef";
+    // string str1("unacademy");
+    // string str2(5, 's');
+    // string str3;
+    // loop(i, 0, 5) {
+    // 	str3.push_back('r');
+    // }
+    // log(str, str1, str2, str3);
+    // log(str[0], str.at(1));
 
-    vector<int> v1(10, -1);
-    logarr(v1, 0, v1.size() - 1);
-    vector<int> v2 {10, 20, 30};
-    vector<int> v3 (v2.begin(), v2.end());
-    logarr(v3, 0, v3.size() - 1);
-    // vector<int> v4(arr, arr+x); x -> index value
+    // str.swap(str1);
+    // log(str, str1);
+    // log(str.size(), str.capacity());
 
-    for(int i = 0; i < v1.size(); i++) {
-        cout<<v1[i]<<" ";
-    }
-    cout<<endl;
-    v1[2] = 23;
-    for(int i = 0; i < v1.size(); i++) {
-        cout<<v1[i]<<" ";
-    }
-    // for-each
-    cout<<endl;
+    // string str;
+    // int n;
+    // cin>>n;
+    // while(n--) {
+    // 	cin>>str;
+    // 	cout<<str<<endl;
+    // }
 
-    for(auto el: v1) {
-        cout<<el<<" ";
-    }
-    // two arrays compare ?????
-    cout<<((v3 == v2) ? "true" : "false");
-    // all true// all false
+    // taking input till eof and multiword input
+    // string str;
+    // while(getline(cin, str)) {
+    // 	cout<<str<<endl;
+    // }
+    // int n;
+    // while(scanf("%d", &n) != EOF) {
+    // 	cout<<n<<" ";
+    // }
 
-    vector<bool> arr;
-    for(int i = 0 ; i < 5; i++) {
-        arr.push_back(true);
-    }
+    // while(cin>>n) {
+    // 	cout<<n<<" ";
+    // }
 
-    cout<<((arr == vector<bool> (5, true)? "\ntrue" : "\nfalse"));
-    v.clear();
-    cout<<"\n"<<v.size();
-    cout<<(v.empty());
+    // string a = "sanket";
+    // string b = "singh";
+    // string c = "sanket";
+    // cout<< (a == b) <<" "<<(a == c);
+
+    // string tokenization
+    string str; cin>>str;
+    istringstream var(str);
+    string token;
+    while(getline(var, token, '-')) {
+        cout<<token<<"\n";
+    }
     return 0;
 }
