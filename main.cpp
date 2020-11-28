f8d57225c9b1df4b2872ed60526dd4b040d6adca
Soumyadip <soumyadip.saha@iiitb.org>
2020-11-28 19:59:28 +0530

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 
-int main(int, char**) {
+void demo(std::vector<int> *temp)
+{
+    std::cout << &temp->at(0) << std::endl;
+}
+
+int main(int, char **)
+{
     std::cout << "Hello, world!\n";
+
+    std::vector<int> arr = {1, 2, 3};
+    std::vector<int> *ptr_arr = &arr;
+
+    std::cout << arr[0] << "\t" << &arr << "\t" << ptr_arr << " \t" << std::endl;
+
+    demo(ptr_arr);
+
+    int x = 10;
+    int &y = x;
+
     return 0;
 }
