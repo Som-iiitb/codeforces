202bc8b740fe7455509db81955bcbd3399ae6cad
Soumyadip <soumyadip.saha@iiitb.org>
2020-11-28 10:17:14 +0530

    Edited test-2.c

--- test-2.c
+++ test-2.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void func (unsigned x) {
-    printf("\n%lu\n", sizeof(x));
+static void func(unsigned x)
+{
+    if (x == 0){
+        return;
+    }
+
+    printf("Hello World inside func\n");
+
+    static int stat = 8;
+    unsigned int x2 = 6;
+    unsigned *temp = malloc(2 * sizeof(size_t));
+
+    *(temp) = x2;
+    *(temp + 1) = x2 + 1;
+
+    func(stat--);
+
+    printf("\na(temp)=%p   a(temp+1)=%p  a(stat)=%p  a(x)=%p  a(x2)=%p  a(func)=%p\n",
+           temp, (temp + 1), &stat, &x, &x2, &func);
+
+    printf("\nsizeof(x)=%lu\t  address(x)=%p\n", sizeof(x), &x);
 }
 
 int main(int argc, char **argv)
 {
-    printf("Hello World\n");
-    
+    printf("\n\n\nHello World inside main\n");
+
     static int stat = 8;
     unsigned int x = 6;
-    unsigned *temp = malloc(2*sizeof(size_t));
-    
+    unsigned *temp = malloc(2 * sizeof(size_t));
+
     *(temp) = x;
-    *(temp+1) = x+1;
-    
+    *(temp + 1) = x + 1;
+
     func(*temp);
-    
-    printf("\n%p\t \t %p \t%p \t %p\t %p \t %p\n", temp, (temp+1), &stat, &x, &main, &func);
-    
+
+    printf("\na(temp)=%p  a(temp+1)=%p  a(stat)=%p  a(x)=%p  a(main)=%p  a(func)=%p\n",
+           temp, (temp + 1), &stat, &x, &main, &func);
+
     return 0;
 }
-
